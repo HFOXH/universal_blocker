42f1ae409a01d1077ba64d0ffaf5f857c419dac0
HFOXH <oscv200@gmail.com>
2025-07-24 20:21:26 -0500

    Create Ionic App

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'universal_blocker',
+  webDir: 'dist'
+};
+
+export default config;
